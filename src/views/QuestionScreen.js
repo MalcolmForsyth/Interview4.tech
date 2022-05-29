cce6565fab2fe4f0c46fb6bc9ebb14b301ca852f
William Eustis <willceustis@gmail.com>
2022-05-29 07:36:50 -0400

    openai q gen

--- src/views/QuestionScreen.js
+++ src/views/QuestionScreen.js
@@ -9,6 +9,33 @@ import ProgressBar from '../components/ProgressBar/ProgressBar';
 import ParticlesBg from 'particles-bg';
 // import Transcibed from './globals';
 import {Transcibed} from '../components/RecButton/RecButton';
+var prompt = `Eric is an interviewer asking questions to a candidate for the position of ${JOB_CAREER}. Eric usually asks questions that build upon the last response that the candidate gave, but can also change the topic and ask a more general question. Questions should be short, well-phrased, and use key information from the candidates last response. 
+
+Provide interview questions to the candidate.
+
+The candidate is Grant.
+Eric: What job title are you applying for?
+Grant: Software Engineer 
+Eric: What are your thoughts on the role of testing in software engineering? 
+Grant: I think a comprehensive testing suite is an important part of any project. In my past internship at Amazon I wrote a suite of automated tests for their home assistant devices.
+Eric: What framework did you use to create automated tests? Was it difficult to learn?
+Grant: I used the Robot framework in Python. It wasn't too difficult to learn, since my mentor was able to guide me through it.
+Eric: Great, can you tell me more about what you were writing the tests for?
+
+The candidate is William.
+Eric: What job title are you applying for?
+William: Machine Learning Engineer 
+Eric: What is overfitting? How can you prevent it? 
+William: Overfitting is when your model finds spurious correlations in your training dataset that lead to poor generalization and poor accuracy in the testing dataset.
+Eric: Name a time that you encountered overfitting and overcame it.
+William: I recently had difficulty with overfitting when doing image classification. I was able to prevent it through strong regularization and data augmentation.
+Eric: Why does data augmentation help with overfitting?
+William: It can help the model rely on more general features which remain under some set of transformations, while spurious features are destroyed under certain transformations.
+Eric: What is the difference between supervised and unsupervised learning?
+
+The candidate is Malcolm.
+Eric: What job title are you applying for?
+Malcolm:`
 
 function QuestionScreen(props) {
     const { title } = props;
@@ -16,42 +43,39 @@ function QuestionScreen(props) {
     const [question, setQuestion] = useState(first_question);
     const [questions, setQuestions] = useState([first_question]);
     const [timerstart, timerreset] = useState([1]);
-    const cohere = require("cohere-ai");
-    cohere.init('tqDPnl8QyMk4HmCHRRR2VL3ns94BecutsbQARYqx');
-    const last_response = "";
-    
-    const model_name = "xlarge";
-    var JOB_CAREER = "";
-    const prompt = `Eric is an interviewer asking questions to a candidate for the position of ${JOB_CAREER}. Eric usually asks questions that build upon the last response that the candidate gave, but can also change the topic and ask a more general question. Questions should be short, well-phrased, and use key information from the candidates last response. Provide interview questions to the candidate.`
-    const examples = "Position: Software Engineer \n The candidate is Grant. \n Eric: What are your thoughts on the role of testing in software engineering? \n Grant: I think a comprehensive testing suite is an important part of any project. In my past internship at Amazon I wrote a suite of automated tests for their home assistant devices. \n Eric: What framework did you use to create automated tests? Was it difficult to learn? \n Grant: I used the Robot framework in Python. It wasn't too difficult to learn, since my mentor was able to guide me through it.\n Eric: Great, can you tell me more about what you were writing the tests for? \n Position: Machine Learning Engineer \n The candidate is William. \n Eric: What is overfitting? How can you prevent it? \n William: Overfitting is when your model finds spurious correlations in your training dataset that lead to poor generalization and poor accuracy in the testing dataset. \n Eric: Name a time that you encountered overfitting and overcame it. \n William: I recently had difficulty with overfitting when doing image classification. I was able to prevent it through strong regularization and data augmentation.\n Eric: Why does data augmentation help with overfitting? \n William: It can help the model rely on more general features which remain under some set of transformations, while spurious features are destroyed under certain transformations. \n Eric: What is the difference between supervised and unsupervised learning? \n Position {} \n The candidate is Malcolm."
-    // const full = prompt + examples + Transcibed.text;
-    // console.log(full);
-    async function response(prev_question) {
-        console.log('Begin Response Func');
-        const end = `\n Eric: ${first_question} \n Malcolm: ${Transcibed.text} \n Eric:`
-        console.log(prompt + examples + end);
-        const response = await cohere.generate(model_name, {
-            prompt: prompt + examples + Transcibed.text,
-            max_tokens: 40,
-            temperature: 0.9,
-            frequency_penalty: 0.5,
-            presence_penalty: 1.0,
-            p: 0.8,
-            stop_sequences: ['Response']
-        });
-        console.log("new Q!");
-        return response
-    }
 
+    const { Configuration, OpenAIApi } = require("openai");
+        
+    const configuration = new Configuration({
+        apiKey: 'sk-',
+    });
+    const openai = new OpenAIApi(configuration);
+
+
+    async function getNextQ(prompt){
+        const response_p = await openai.createCompletion("text-davinci-002", {
+            prompt: prompt,
+            max_tokens: 120,
+            temperature: .8,
+            top_p: .8,
+            frequency_penalty: 2.0,
+            stop: ['Malcolm:']
+        })
+        return response_p
+    }
+    
     const next_question = () => {
-        response(question).then((res) =>
-        {
-           // console.log(response);
-            console.log(res);
-            console.log("waited");
-            //  response.body.generations[0].text;
-            setQuestion(res.body.generations[0].text);
-        });
+        // make new prompt
+        // generate new Q
+        prompt = prompt + Transcibed.text;
+        prompt = prompt + "\n";
+        prompt =prompt + "Eric:"
+        question = getNextQ(prompt).then(resp => resp.data.choices.text)
+        prompt = prompt + question;
+        prompt = prompt + "\n";
+        prompt = prompt + "Malcolm:";
+
+        setQuestion(question);
     };
 
     const reset = () => {
