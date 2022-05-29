ada75b1a2abb62c31cf34267d844b3e9cad2d24a
ghoey <ghoey2@illinois.edu>
2022-05-29 10:20:55 -0500

    formating score board with real data

--- src/components/ScoreCard/ScoreCard.js
+++ src/components/ScoreCard/ScoreCard.js
@@ -26,18 +26,28 @@ class ScoreCard extends React.Component {
     render() {
         if (typeof Transcribed.text == 'undefined'){
             // console.log("Transcribed Undefined")
+            
+
+            var feedback = ""
+            feedback = feedback + scores[0][1] + " "
+            feedback = feedback + scores[1][1] + ". "
+            feedback = feedback + scores[2][1] + ". "
+            feedback = feedback + scores[3][1] + ". "
             return (
-                <div> 
-                    <h1 className="scoretitletext">Your Scores</h1>
-                    <div className="displaywindow">
-                        <NewProgBar className='displaybar' value={scores[0][0]} title={scores[0][1]} /> 
-                        <NewProgBar className='displaybar' value={scores[1][0]} title={scores[1][1]} /> 
-                        <NewProgBar className='displaybar' value={scores[2][0]} title={scores[2][1]} /> 
-                        <NewProgBar className='displaybar' value={scores[3][0]} title={scores[3][1]} /> 
-                    
-                    </div>
+            <div> 
+                <h1 className="scoretitletext">Your Scores</h1>
+                <div className="displaywindow">
+                    <NewProgBar className='displaybar' value={(scores[0])[0]} title="name of score" /> 
+                    <NewProgBar className='displaybar' value={(scores[1])[0]} title="does this work" /> 
+                    <NewProgBar className='displaybar' value={(scores[2])[0]} title="another thing" /> 
+                    <NewProgBar className='displaybar' value={(scores[3])[0]} title="the final thing" /> 
+                
                 </div>
-            );
+                <h1 className="scoretitletext">FeedBack:</h1>
+                <h1 className="feedbacktext">{feedback}</h1>
+
+            </div>
+            )
         }
         
         else if (CURR_Q != rendered_q){
@@ -47,36 +57,51 @@ class ScoreCard extends React.Component {
             rendered_q = CURR_Q;
         }
         else{
-            // console.log("??")
+            var feedback = ""
+            feedback = feedback + scores[0][1] + " "
+            feedback = feedback + scores[1][1] + ". "
+            feedback = feedback + scores[2][1] + ". "
+            feedback = feedback + scores[3][1] + ". "
             return (
-                <div> 
-                    <h1 className="scoretitletext">Your Scores</h1>
-                    <div className="displaywindow">
-                        <NewProgBar className='displaybar' value={scores[0][0]} title={scores[0][1]} /> 
-                        <NewProgBar className='displaybar' value={scores[1][0]} title={scores[1][1]} /> 
-                        <NewProgBar className='displaybar' value={scores[2][0]} title={scores[2][1]} /> 
-                        <NewProgBar className='displaybar' value={scores[3][0]} title={scores[3][1]} /> 
-                    
-                    </div>
+            <div> 
+                <h1 className="scoretitletext">Your Scores</h1>
+                <div className="displaywindow">
+                    <NewProgBar className='displaybar' value={(scores[0])[0]} title="name of score" /> 
+                    <NewProgBar className='displaybar' value={(scores[1])[0]} title="does this work" /> 
+                    <NewProgBar className='displaybar' value={(scores[2])[0]} title="another thing" /> 
+                    <NewProgBar className='displaybar' value={(scores[3])[0]} title="the final thing" /> 
+                
                 </div>
-            );
+                <h1 className="scoretitletext">FeedBack:</h1>
+                <h1 className="feedbacktext">{feedback}</h1>
+
+            </div>
+            )
         }
         console.log(scores_p)   
         return scores_p.then(scores_new => {
             console.log(CURR_Q, rendered_q, scores, scores_new)
             scores = scores_new
+            var feedback = ""
+            feedback = feedback + scores[0][1] + " "
+            feedback = feedback + scores[1][1] + ". "
+            feedback = feedback + scores[2][1] + ". "
+            feedback = feedback + scores[3][1] + ". "
             return (
-                <div> 
-                    <h1 className="scoretitletext">Your Scores</h1>
-                    <div className="displaywindow">
-                        <NewProgBar className='displaybar' value={scores[0][0]} title={scores[0][1]} /> 
-                        <NewProgBar className='displaybar' value={scores[1][0]} title={scores[1][1]} /> 
-                        <NewProgBar className='displaybar' value={scores[2][0]} title={scores[2][1]} /> 
-                        <NewProgBar className='displaybar' value={scores[3][0]} title={scores[3][1]} /> 
-                    
-                    </div>
+            <div> 
+                <h1 className="scoretitletext">Your Scores</h1>
+                <div className="displaywindow">
+                    <NewProgBar className='displaybar' value={(scores[0])[0]} title="name of score" /> 
+                    <NewProgBar className='displaybar' value={(scores[1])[0]} title="does this work" /> 
+                    <NewProgBar className='displaybar' value={(scores[2])[0]} title="another thing" /> 
+                    <NewProgBar className='displaybar' value={(scores[3])[0]} title="the final thing" /> 
+                
                 </div>
-            );
+                <h1 className="scoretitletext">FeedBack:</h1>
+                <h1 className="feedbacktext">{feedback}</h1>
+
+            </div>
+            )
         })
         // return (
         //     <div> 
